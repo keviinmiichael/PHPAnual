dd8f82ffb2f50849fb91d06cd6e540a76a191146
Kevin Ghio <kevin@MacBook-Pro-de-Kevin.local>
2018-05-31 22:11:51 -0300

    clase 6 y 7

--- perfil.php
+++ perfil.php
@@ -1,12 +1,13 @@
-
-
-
-<?php
-
-$usuarios = ['Ana','Kevin', 'Manu'];
-
-
- var_dump($usuarios[$_GET['id']]);
-
-
-  ?>
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+    <head>
+        <meta charset="utf-8">
+        <title></title>
+    </head>
+    <body>
+        Registrate
+        <a href="clase06.php">aca, si sos vendedor</a>
+        <br><br>
+        <a href="perfil.php?id=1">aca, si sos cliente</a>
+    </body>
+</html>
